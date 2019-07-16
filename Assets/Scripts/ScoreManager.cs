a0ec6fba3d72706d1fef0a37da65e14b90f04941
cy-kenta_kitagawa <kenta_kitagawa@cybird.co.jp>
2019-07-16 23:06:36 +0900

    UIの追加

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private static ScoreManager instance = null;
+
+    private static int score = 0;
+
+    public static ScoreManager GetInstance()
+    {
+        if (instance != null) return instance;
+
+        GameObject singletonObj = new GameObject("ScoreManager");
+        DontDestroyOnLoad(singletonObj);
+        instance = (ScoreManager)singletonObj.AddComponent(typeof(ScoreManager));
+        return instance;
+    }
+
+
+    public static int AddScore(int _score)
+    {
+        score += _score;
+        return score;
+    }
+}
